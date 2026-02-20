2bf8fdd68209974ddceaf64f4785c84905118746
Molokoda <36948089+Molokoda@users.noreply.github.com>
2026-02-20 12:53:06 +0300

    feat: added separator component to flat list

---
+++ shared/ui/Separator.tsx
@@ -0,0 +1,19 @@
+import { memo } from 'react';
+
+import { View, StyleSheet } from 'react-native';
+
+import { theme } from '../config';
+
+const SeparatorBase = () => {
+  return <View style={styles.separator} />;
+};
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 1,
+    backgroundColor: theme.colors.textMuted,
+    opacity: 0.4,
+  },
+});
+
+export const Separator = memo(SeparatorBase);

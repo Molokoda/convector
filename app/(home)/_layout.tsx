6ac7e07521d8de0896e28a8d69ecf099dc9e6d86
Molokoda <36948089+Molokoda@users.noreply.github.com>
2026-02-20 10:19:37 +0300

    fix: changed layout name

--- app/(home)/_layout.tsx
+++ app/(home)/_layout.tsx
@@ -3,7 +3,7 @@ import { Tabs } from 'expo-router';
 
 import { theme } from '@/shared';
 
-export default function RootLayout() {
+export default function HomeLayout() {
   return (
     <Tabs
       screenOptions={{

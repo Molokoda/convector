1f178845fef322a2cee65342fecad22b4595a803
Molokoda <36948089+Molokoda@users.noreply.github.com>
2026-02-21 17:28:35 +0300

    reafactor: added initial data for store

--- shared/store/currenciesStore.ts
+++ shared/store/currenciesStore.ts
@@ -7,7 +7,26 @@ interface CurrenciesState {
   setCurrencies: (items: Rate[]) => void;
 }
 
+const initialCurrencies: Rate[] = [
+  {
+    Cur_ID: 431,
+    Date: '2026-02-21T00:00:00',
+    Cur_Abbreviation: 'USD',
+    Cur_Scale: 1,
+    Cur_Name: 'Доллар США',
+    Cur_OfficialRate: 2.8517,
+  },
+  {
+    Cur_ID: 451,
+    Date: '2026-02-21T00:00:00',
+    Cur_Abbreviation: 'EUR',
+    Cur_Scale: 1,
+    Cur_Name: 'Евро',
+    Cur_OfficialRate: 3.365,
+  },
+];
+
 export const useCurrenciesStore = create<CurrenciesState>((set) => ({
-  currencies: [],
+  currencies: initialCurrencies,
   setCurrencies: (items) => set({ currencies: items }),
 }));
